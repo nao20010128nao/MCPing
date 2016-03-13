d502b581413ebf7bd5260e33bcc3d71c951ea0b3
nao20010128nao <nao20010128@gmail.com>
2016-03-14 00:11:53 +0900

    ファイルサイズ軽量化

--- src/com/nao20010128nao/MCPing/pc/Reply.java
+++ src/com/nao20010128nao/MCPing/pc/Reply.java
@@ -2,13 +2,11 @@ package com.nao20010128nao.MCPing.pc;
 
 import java.util.List;
 
-import com.nao20010128nao.MCPing.ServerPingResult;
-
 /**
  * References: http://wiki.vg/Server_List_Ping
  * https://gist.github.com/thinkofdeath/6927216
  */
-public class Reply implements ServerPingResult,PCQueryResult {
+public class Reply implements PCQueryResult {
 
 	public String description;
 	public Players players;
--- src/com/nao20010128nao/MCPing/pc/Reply19.java
+++ src/com/nao20010128nao/MCPing/pc/Reply19.java
@@ -2,15 +2,13 @@ package com.nao20010128nao.MCPing.pc;
 
 import java.util.List;
 
-import com.nao20010128nao.MCPing.ServerPingResult;
-
 /**
  * References: http://wiki.vg/Server_List_Ping
  * https://gist.github.com/thinkofdeath/6927216
  *
  * Modified for MC 1.9
  */
-public class Reply19 implements ServerPingResult, PCQueryResult {
+public class Reply19 implements PCQueryResult {
 
 	public Description description;
 	public Players players;
